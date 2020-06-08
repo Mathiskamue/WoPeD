7c3a0bcb1d3f7d8b6cab17ba52bd5e360f474bbe
martinjonathan <jonathan.martin@protonmail.com>
2020-06-08 10:42:31 +0200

    Revert "Revert "Gruppe zu Texterstellung hinzugefügt""
    This reverts commit 9f7693dc633e04d556f4be9ac913b2751c3730c7.

--- WoPeD-Process2Text/src/org/woped/p2t/dataModel/intermediate/ExecutableFragment.java
+++ WoPeD-Process2Text/src/org/woped/p2t/dataModel/intermediate/ExecutableFragment.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class ExecutableFragment extends AbstractFragment {
     private final ArrayList<ExecutableFragment> sentenceList;
 
-    public ExecutableFragment(String action, String bo, String role, String addition) {
-        super(action, bo, role, addition);
+    public ExecutableFragment(String action, String bo, String role, String group, String addition) {
+        super(action, bo, role, group, addition);
         sentenceList = new ArrayList<>();
     }
 
